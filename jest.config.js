b84bfed319c3e653b29b93e48e964c150a775fc3
gonnbar <gonzalonicolasbarbosa@gmail.com>
2026-08-31 20:25:12 -0300

    update package.json

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  setupFilesAfterEnv: ['./lib/models/index.js', './jest.setup.js'],
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ['./node_modules', './dist'],
-  modulePathIgnorePatterns: ['./docker'],
-};
